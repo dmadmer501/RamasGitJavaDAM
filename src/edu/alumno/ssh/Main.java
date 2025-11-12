bfdec7ed36e5e0501091fd0bd65f9f676862d9bc
dmadmer501 <dmadmer501@g.educaand.es>
2025-11-12 19:29:12 +0100

    Proyecto Java creado desde Eclipse y gestionado por terminal (SSH listo)

---
+++ src/edu/alumno/ssh/Main.java
@@ -0,0 +1,22 @@
+package edu.alumno.ssh;
+
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		// Investiga el uso de random y genera por pantalla los números de la lotería
+		// primitiva.
+		Random random = new Random();
+
+		System.out.println("Números de la Lotería Primitiva:");
+		// Genera 5 números principales
+		for (int i = 0; i < 5; i++) {
+			int numero = random.nextInt(49) + 1;
+			System.out.print(numero + " ");
+		}
+
+		// Número complementario
+		int complementario = random.nextInt(49) + 1;
+		System.out.println("\nComplementario: " + complementario);
+	}
+}
